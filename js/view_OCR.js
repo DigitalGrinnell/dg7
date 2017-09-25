019520ad84c6b2b7b70935ec436a66ac37b77507
Mark McFate <mcfatem@grinnell.edu>
2017-09-25 09:03:01 -0500

    View OCR additions

---
+++ js/view_OCR.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mcfatem on 09/19/2017.
+ */
+
+function dg7ViewOCR(address) {
+    window.open( address, "_blank", "toolbar=0,location=0,menubar=0,resizable=1,width=600,height=900" );
+    return false;
+};
