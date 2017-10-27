f02eb887d9a5084ec021dab81dbc1c1ccf12842d
Mark McFate <mcfatem@grinnell.edu>
2017-10-27 10:29:01 -0500

    Tweaked dg7NewWindow width

--- js/open_transcription_window.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by mcfatem on 12/23/2014.
- */
-function dg7OpenTranscriptionWindow(address) {
-    window.open( address, "_blank", "toolbar=0,location=0,menubar=0,resizable=1,width=950,height=900,scrollbars=1" );
-    return false;
-};
